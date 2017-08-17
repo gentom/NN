763c3b4dd3fb73d09ede193d21ca29bef88a1ff4
gentom <gento.m.developer@gmail.com>
2017-08-17 15:53:46 +0900

    commit

--- basics/train_network.py
+++ basics/train_network.py
@@ -3,3 +3,14 @@
 import init_network
 import forward_propagation
 import back_propagation
+
+
+def update_weights(network, row, l_rate):
+    for i in range(len(network)):
+        inputs = row[:-1]
+        if i != 0:
+            inputs = [neuron['output'] for neuron in network[i - 1]]
+        for neuron in network[i]:
+            for j in range(len(inputs)):
+                neuron['weights'][j] += l_rate * neuron['delta'] * inputs[j]
+            neuron['weights'][-1] += l_rate * neuron['delta']
